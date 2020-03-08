652e65118eb487f5b3f7c16a47042917a24a2809
getherefast <getherefast18@gmail.com>
2020-03-07 23:48:09 -0300

    Updt 05

--- 0301v1.py
+++ 0301v1.py
@@ -1,6 +1,12 @@
 T = int(input('Quantidade de anos fumando: '))
 Ci = int(input('Quantidade de cigarros por dia: '))
 PCa = float(input('Preço da carteira de cigarros: '))
-QtCa = Ci * T * 365 // 20
-Qt = QtCa * PCa
+QtCi = Ci * T * 365
+QtCa = QtCi // 20
+Y = QtCi % 20
+if Y > 0 :
+    X = PCa
+else :
+    X = 0
+Qt = QtCa * PCa + X
 print('Dinheiro gasto em cigarros em', T ,'anos:', Qt)
---
+++ 0304v3.py
@@ -0,0 +1,9 @@
+Qt = float(input('Quantidade a sacar: '))
+Q50 = Qt // 50.0
+Qt %= 50.0
+Q10 = Qt // 10.0
+Qt %= 10.0
+Q5 = Qt // 5.0
+Qt %= 5.0
+Q2 = Qt // 2.0
+print('Quantidade respectiva de notas de 50.0, 10.0, 5.0 e 2.0 reais a ser retiradas: ', Q50, Q10, Q5, Q2)
